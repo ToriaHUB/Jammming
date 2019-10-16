d30c673ff9a23f043cf7a6856834ee6d6bb62221
Viktoria Petrenko <vikuacz@seznam.cz>
2019-10-16 21:44:11 +0200

    jammming: passed the playlist name and tracks from the App component to the Playlist component.

--- src/Components/Playlist/Playlist.tsx
+++ src/Components/Playlist/Playlist.tsx
@@ -1,8 +1,14 @@
 import React from "react"
 import "./Playlist.css"
 import { TrackList } from "../TrackList/TrackList"
+import { TrackType } from "../../types"
 
-export const Playlist = () => {
+type Props = {
+  playlistName: string
+  playlistTracks: TrackType[]
+}
+
+export const Playlist = (props: Props) => {
   return (
     <div className="Playlist">
       <input defaultValue={"New Playlist"} />
